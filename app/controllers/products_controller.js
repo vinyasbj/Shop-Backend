73ae452bfc010023f37f228821aaa3f44a2455e4
vinyasbj <vinyas555@gmail.com>
2019-03-26 21:13:40 +0530

    few changes

--- app/controllers/products_controller.js
+++ app/controllers/products_controller.js
@@ -61,6 +61,18 @@ router.delete('/:id', validateID, (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    let body = req.body; 
+    let product = new Product(body); 
+    product.save().then((product) => {
+        res.send({
+            product, 
+            notice: 'Successfully created a product'
+        }); 
+    }).catch((err) => {
+        res.send(err); 
+    })
+})
 
 // products/id/short
 // info of product like the name id and price
