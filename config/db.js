1ce929dbc48010d178f56a6f2cfe0f4a3c52b218
vinyasbj <vinyas555@gmail.com>
2019-03-26 20:54:23 +0530

    prod url

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
+const PORT = process.env.PORT || 5000;
 const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://heroku_p8v78z7c:ocuuuc2gdt3fsiqmk2ue3oo9ua@ds223756.mlab.com:23756/heroku_p8v78z7c';
 mongoose.connect('mongodb://heroku_p8v78z7c:ocuuuc2gdt3fsiqmk2ue3oo9ua@ds223756.mlab.com:23756/heroku_p8v78z7c', { useNewUrlParser: true }).then((res) => {
     console.log('connected to db'); 
