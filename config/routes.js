36a655c6ae5c31e9412a4fcf58cbe41b6273d318
vinyasbj <vinyas555@gmail.com>
2019-03-26 21:08:14 +0530

    few changes

--- config/routes.js
+++ config/routes.js
@@ -6,7 +6,7 @@ const { productsController } = require('../app/controllers/products_controller')
 const { usersController } = require('../app/controllers/users_controller')
 const { ordersController } = require('../app/controllers/orders_controller')
 
-router.use('/books', booksController); 
+// router.use('/books', booksController); 
 router.use('/categories', categoriesController); 
 router.use('/products', productsController); 
 router.use('/users', usersController)
