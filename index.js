bb0d11cb0dc690bf035cd973823fad0f5fdcf39e
vinyasbj <vinyas555@gmail.com>
2019-03-26 23:31:16 +0530

    cors installed

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 // npm init -y 
 // npm install --save express
 const express = require('express'); 
-var cors = require('cors')
 const app = express(); 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 const { mongoose } = require('./config/db'); 
 const { routes } = require('./config/routes'); 
 // // localhost:3000/books
 
 app.use(express.json()); 
-app.use(cors())
+
 app.use('/', routes); 
 
 app.listen(port, function() {
     console.log('listening on port', port); 
 }); 
-
-app.listen(port, function () {
-    console.log('CORS-enabled web server listening on port 80')
-  })
