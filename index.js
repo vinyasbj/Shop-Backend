c74824773f5c797cc7d1fcfda8fc1e15516ef1e2
vinyasbj <vinyas555@gmail.com>
2019-03-28 20:01:40 +0530

    cors fix

--- index.js
+++ index.js
@@ -3,11 +3,23 @@
 const express = require('express'); 
 const app = express(); 
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 const { mongoose } = require('./config/db'); 
 const { routes } = require('./config/routes'); 
 // // localhost:3000/books
-
 app.use(express.json()); 
+app.use(cors());
+
+// app.use((req,res,next)=>{
+//     res.header("Access-Control-Allow-Origin",'*');
+//     res.header("Access-Control-Allow-Headers",
+//     'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+//     if(req.method === 'OPTIONS'){
+//         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,GET,DELETE');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 app.use('/', routes); 
 
